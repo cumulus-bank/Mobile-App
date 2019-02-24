18f7f6ebb76805bfa42c919e6efa00ecba7f67b3
Saif Ur Rehman <s4saif.121@gmail.com>
2019-02-24 10:29:30 +0400

    #1 auto login fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,35 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { timer } from 'rxjs/observable/timer';
 import { LoginPage } from '../pages/login/login';
+import {TabsPage} from '../pages/tabs/tabs'
+import * as jwtDecode from 'jwt-decode';
+import {Provider} from '../provider/provider'
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any ;
   showSplash = true;
+  public key:any;
   
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public keyboard: Keyboard 
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public keyboard: Keyboard ,  public provider:Provider
     ) {
     platform.ready().then(() => {
+      this.key=localStorage.getItem('token');
+      this.rootPage = LoginPage
+      // console.log('my key isss', this.key)
+      // if(this.key){
+      //   this.provider.userData = jwtDecode(this.key);
+      //   this.rootPage = TabsPage
+      // }
+      // else{
+      //   this.rootPage = LoginPage
+      // }
        
       statusBar.styleDefault();
       splashScreen.hide();
-      timer(1000).subscribe(() => this.showSplash = false) // <-- hide animation after 3s
+      timer(1000).subscribe(() => this.showSplash = false)
     });
   }
 }
